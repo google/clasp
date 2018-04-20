882aff5fd95b6f584a0620ea0429443928586f9e
Campion Fellin <campionfellin@users.noreply.github.com>
2018-04-20 00:11:42 -0700

    Creates tests for clasp clone and clasp pull (#119)
    * Creates tests for clasp clone and clasp pull

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import * as fs from 'fs';
 const { spawnSync } = require('child_process');
 
 describe('Test help for each function', () => {
@@ -43,6 +44,29 @@ describe.skip('Test clasp create function', () => {
   });
 });
 
+describe.skip('Test clasp clone function', () => {
+  it('should clone an existing project correctly', () => {
+    const settings = JSON.parse(fs.readFileSync('.clasp.json', 'utf8'));
+    const result = spawnSync(
+      'clasp', ['clone', settings.scriptId], { encoding: 'utf8' }
+    );
+    expect(result.stdout).to.contain('Cloned');
+    expect(result.stdout).to.contain('files.');
+    expect(result.status).to.equal(0);
+  });
+});
+
+describe.skip('Test clasp pull function', () => {
+  it('should pull an existing project correctly', () => {
+    const result = spawnSync(
+      'clasp', ['pull'], { encoding: 'utf8' }
+    );
+    expect(result.stdout).to.contain('Cloned');
+    expect(result.stdout).to.contain('files.');
+    expect(result.status).to.equal(0);
+  });
+});
+
 /**
  * TODO: Test these commands and configs.
  *
@@ -54,8 +78,8 @@ describe.skip('Test clasp create function', () => {
  * [ ] clasp create "myTitle"
  * [x] clasp create <untitled>
  * [x] clasp list
- * [ ] clasp clone <scriptId>
- * [ ] clasp pull
+ * [x] clasp clone <scriptId>
+ * [x] clasp pull
  * [ ] echo '// test' >> index.js && clasp push
  * [ ] clasp open
  * [ ] clasp deployments
