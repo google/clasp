69661933d5bb61e5c7a66bf2707d81a39b6fa3b2
campionfellin <campionfellin@gmail.com>
2018-05-30 22:32:15 -0700

    Promisify loadAPICredentials
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -68,17 +68,20 @@ async function authorize(useLocalhost: boolean, writeToOwnKey: boolean) {
  * Required before every API call.
  */
 export async function loadAPICredentials() {
-    return DOTFILE.RC_LOCAL.read().then((rc: ClaspSettings) => {
-      oauth2Client.setCredentials(rc);
-      return rc;
-    }).catch((err: any) => {
-      return DOTFILE.RC.read().then((rc: ClaspSettings) => {
+    return new Promise((resolve, reject) => {
+      DOTFILE.RC_LOCAL.read().then((rc: ClaspSettings) => {
         oauth2Client.setCredentials(rc);
-        return rc;
+        resolve();
       }).catch((err: any) => {
-        console.error('Could not read API credentials. Error:');
-        console.error(err);
-        process.exit(1);
+        DOTFILE.RC.read().then((rc: ClaspSettings) => {
+          oauth2Client.setCredentials(rc);
+          resolve();
+        }).catch((err: any) => {
+          console.error('Could not read API credentials. Error:');
+          console.error(err);
+          reject();
+          process.exit(1);
+        });
       });
     });
   }
