00e08136356a741f54ced99d34e93d91524acddc
takanakahiko <takanakahiko@gmail.com>
2018-11-11 19:05:09 +0900

    refactoring

--- src/commands.ts
+++ src/commands.ts
@@ -165,61 +165,49 @@ export const create = async (title: string, parentId: string, cmd: {
  */
 export const clone = async (scriptId: string, versionNumber?: number) => {
   await checkIfOnline();
-  if (hasProject()) {
-    logError(null, ERROR.FOLDER_EXISTS);
+  if (hasProject()) return logError(null, ERROR.FOLDER_EXISTS);
+  if (!scriptId) {
+    await loadAPICredentials();
+    const list = await drive.files.list({
+      // pageSize: 10,
+      // fields: 'files(id, name)',
+      orderBy: 'modifiedByMeTime desc',
+      q: 'mimeType="application/vnd.google-apps.script"',
+    });
+    const data = list.data;
+    if (!data) return logError(list.statusText, 'Unable to use the Drive API.');
+    const files = data.files;
+    if (!files || !files.length) return console.log(LOG.FINDING_SCRIPTS_DNE);
+    const fileIds = files.map((file: any) => {
+      return {
+        name: `${padEnd(file.name, 20)} – ${LOG.SCRIPT_LINK(file.id)}`,
+        value: file.id,
+      };
+    });
+    const answers = await prompt([{
+      type: 'list',
+      name: 'scriptId',
+      message: 'Clone which script? ',
+      choices: fileIds,
+      pageSize: 30,
+    }]);
+    scriptId = answers.scriptId;
   } else {
-    if (!scriptId) {
-      await loadAPICredentials();
-      const list = await drive.files.list({
-        // pageSize: 10,
-        // fields: 'files(id, name)',
-        orderBy: 'modifiedByMeTime desc',
-        q: 'mimeType="application/vnd.google-apps.script"',
+    // We have a scriptId or URL
+    // If we passed a URL, extract the scriptId from that. For example:
+    // https://script.google.com/a/DOMAIN/d/1Ng7bNZ1K95wNi2H7IUwZzM68FL6ffxQhyc_ByV42zpS6qAFX8pFsWu2I/edit
+    if (scriptId.length !== 57) { // 57 is the magic number
+      const ids = scriptId.split('/').filter((s) => {
+        return s.length === 57;
       });
-      const data = list.data;
-      if (!data) return logError(list.statusText, 'Unable to use the Drive API.');
-      const files = data.files;
-      if (files && files.length) {
-        const fileIds = files.map((file: any) => {
-          return {
-            name: `${padEnd(file.name, 20)} – ${LOG.SCRIPT_LINK(file.id)}`,
-            value: file.id,
-          };
-        });
-        await prompt([{
-          type: 'list',
-          name: 'scriptId',
-          message: 'Clone which script? ',
-          choices: fileIds,
-          pageSize: 30,
-        }]).then((answers: any) => {
-          checkIfOnline();
-          spinner.setSpinnerTitle(LOG.CLONING);
-          saveProject(answers.scriptId);
-          fetchProject(answers.scriptId, '', versionNumber);
-        }).catch((err: any) => {
-          console.log(err);
-        });
-      } else {
-        console.log(LOG.FINDING_SCRIPTS_DNE);
+      if (ids.length) {
+        scriptId = ids[0];
       }
-    } else {
-      // We have a scriptId or URL
-      // If we passed a URL, extract the scriptId from that. For example:
-      // https://script.google.com/a/DOMAIN/d/1Ng7bNZ1K95wNi2H7IUwZzM68FL6ffxQhyc_ByV42zpS6qAFX8pFsWu2I/edit
-      if (scriptId.length !== 57) { // 57 is the magic number
-        const ids = scriptId.split('/').filter((s) => {
-          return s.length === 57;
-        });
-        if (ids.length) {
-          scriptId = ids[0];
-        }
-      }
-      spinner.setSpinnerTitle(LOG.CLONING);
-      saveProject(scriptId);
-      fetchProject(scriptId, '', versionNumber);
     }
   }
+  spinner.setSpinnerTitle(LOG.CLONING);
+  saveProject(scriptId);
+  fetchProject(scriptId, '', versionNumber);
 };
 
 /**
@@ -353,17 +341,14 @@ export const logs = async (cmd: {
         }]).then((answers: any) => {
           projectId = answers.projectId;
           const dotfile = DOTFILE.PROJECT();
-          if (dotfile) {
-            dotfile.read().then((settings: ProjectSettings) => {
-              if (!settings.scriptId) logError(ERROR.SCRIPT_ID_DNE);
-              dotfile.write(Object.assign(settings, { projectId }));
-              resolve(projectId);
-            }).catch((err: object) => {
-              reject(logError(err));
-            });
-          } else {
-            reject(logError(null, ERROR.SETTINGS_DNE));
-          }
+          if (!dotfile) return reject(logError(null, ERROR.SETTINGS_DNE));
+          dotfile.read().then((settings: ProjectSettings) => {
+            if (!settings.scriptId) logError(ERROR.SCRIPT_ID_DNE);
+            dotfile.write(Object.assign(settings, { projectId }));
+            resolve(projectId);
+          }).catch((err: object) => {
+            reject(logError(err));
+          });
         }).catch((err: any) => {
           reject(console.log(err));
         });
@@ -473,25 +458,24 @@ export const run = async (functionName: string, cmd: { nondev: boolean }) => {
       },
     });
     spinner.stop(true);
-    if (res && res.data.done) {
-      const data = res.data;
-      // @see https://developers.google.com/apps-script/api/reference/rest/v1/scripts/run#response-body
-      if (data.response) {
-        if (data.response.result) {
-          console.log(data.response.result);
-        } else {
-          console.log(chalk.red('No response.'));
-        }
-      } else if (data.error && data.error.details) {
-        // @see https://developers.google.com/apps-script/api/reference/rest/v1/scripts/run#Status
-        console.error(`${chalk.red('Exception:')}`,
-          data.error.details[0].errorType,
-          data.error.details[0].errorMessage,
-          data.error.details[0].scriptStackTraceElements || []);
-      }
-    } else {
+    if (!res || !res.data.done) {
       logError(null, ERROR.RUN_NODATA);
       process.exit(0); // exit gracefully in case localhost server spun up for authorize
+    }
+    const data = res.data;
+    // @see https://developers.google.com/apps-script/api/reference/rest/v1/scripts/run#response-body
+    if (data.response) {
+      if (data.response.result) {
+        console.log(data.response.result);
+      } else {
+        console.log(chalk.red('No response.'));
+      }
+    } else if (data.error && data.error.details) {
+      // @see https://developers.google.com/apps-script/api/reference/rest/v1/scripts/run#Status
+      console.error(`${chalk.red('Exception:')}`,
+        data.error.details[0].errorType,
+        data.error.details[0].errorMessage,
+        data.error.details[0].scriptStackTraceElements || []);
     }
   } catch(err) {
     spinner.stop(true);
