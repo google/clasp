ebbc6cb91a5407f20e8900266a0696f43efd3fc5
Campion Fellin <campionfellin@users.noreply.github.com>
2018-12-14 16:46:21 -0800

    Refactor common test code (#463)
    * Refactor common test code
    
    Signed-off-by: campionfellin <campionfellin@gmail.com>
    
    * Fix up testing files
    
    Signed-off-by: campionfellin <campionfellin@gmail.com>
    
    * Fix setup functions
    
    Signed-off-by: campionfellin <campionfellin@gmail.com>
    
    * Fix typings
    
    Signed-off-by: campionfellin <campionfellin@gmail.com>

---
+++ tests/constants.ts
@@ -0,0 +1,87 @@
+import * as os from 'os';
+import * as path from 'path';
+
+import {
+  rndStr,
+} from './functions';
+import { ClaspToken } from '../src/dotfile';
+import { OAuth2ClientOptions } from 'google-auth-library';
+
+// Sample files
+export const TEST_CODE_JS = 'function test() { Logger.log(\'test\'); }';
+export const TEST_APPSSCRIPT_JSON = JSON.stringify({timeZone: 'America/New_York'});
+
+// Travis Env Variables
+export const IS_PR: boolean = (process.env.TRAVIS_PULL_REQUEST === 'true');
+export const SCRIPT_ID: string = process.env.SCRIPT_ID || '';
+const PROJECT_ID: string = process.env.PROJECT_ID || '';
+const HOME: string = process.env.HOME || '';
+
+// Paths
+export const CLASP_PATHS = {
+  clientCredsLocal: 'client_credentials.json',
+  rcGlobal: path.join(HOME, '.clasprc.json'),
+  rcLocal: '.clasprc.json',
+  settingsLocal: '.clasp.json',
+};
+
+// Other constants
+export const CLASP: string = (os.type() === 'Windows_NT') ? 'clasp.cmd' : 'clasp';
+export const CLASP_USAGE = 'Usage: clasp <command> [options]';
+
+const VALID_CLASP_SETTINGS = {
+  scriptId: SCRIPT_ID,
+  projectId: PROJECT_ID,
+};
+
+const INVALID_CLASP_SETTINGS = {
+  scriptId: SCRIPT_ID,
+  projectId: `project-id-${rndStr()}`,
+};
+
+export const CLASP_SETTINGS = {
+  valid: JSON.stringify(VALID_CLASP_SETTINGS),
+  invalid: JSON.stringify(INVALID_CLASP_SETTINGS),
+};
+
+const FAKE_CLASPRC_TOKEN = {
+  access_token: rndStr(),
+  refresh_token: rndStr(),
+  scope: 'https://www.googleapis.com/auth/script.projects',
+  token_type: 'Bearer',
+  expiry_date: (new Date()).getTime(),
+};
+
+const oAuth2ClientOptions: OAuth2ClientOptions = {
+  clientId: `${rndStr()}.apps.googleusercontent.com`,
+  clientSecret: rndStr(),
+};
+
+const FAKE_CLASPRC_LOCAL: ClaspToken = {
+  token: FAKE_CLASPRC_TOKEN,
+  oauth2ClientSettings: oAuth2ClientOptions,
+  isLocalCreds: true,
+};
+
+export const FAKE_CLASPRC = {
+  local: JSON.stringify(FAKE_CLASPRC_LOCAL),
+  token: JSON.stringify(FAKE_CLASPRC_TOKEN),
+};
+
+const FAKE_CLIENT_CREDS = {
+  installed: {
+    client_id: `${rndStr()}.apps.googleusercontent.com`,
+    client_secret: rndStr(),
+  },
+};
+
+const INVALID_CLIENT_CREDS = {
+  installed: {
+    client_id: `${rndStr()}.apps.googleusercontent.com`,
+  },
+};
+
+export const CLIENT_CREDS = {
+  fake: JSON.stringify(FAKE_CLIENT_CREDS),
+  invalid: JSON.stringify(INVALID_CLIENT_CREDS),
+};
---
+++ tests/functions.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs-extra';
+import {
+  CLASP_SETTINGS,
+  CLASP_PATHS,
+  TEST_APPSSCRIPT_JSON,
+} from './constants';
+
+const copyFileSync = require('fs-copy-file-sync');
+
+export const cleanup = () => {
+  fs.removeSync('.clasp.json');
+  fs.removeSync('.claspignore');
+  fs.removeSync('Code.js');
+  fs.removeSync('appsscript.json');
+};
+
+export const setup = () => {
+  fs.writeFileSync('.clasp.json', CLASP_SETTINGS.valid);
+  fs.writeFileSync('appsscript.json', TEST_APPSSCRIPT_JSON);
+};
+
+export const rndStr = () => Math.random().toString(36).substr(2);
+
+export const backupSettings = () => {
+  // fs.copyFileSync isn't supported in Node 6/7
+  if (fs.existsSync(CLASP_PATHS.rcGlobal)) {
+    copyFileSync(CLASP_PATHS.rcGlobal, `${CLASP_PATHS.rcGlobal}~`);
+  }
+  if (fs.existsSync(CLASP_PATHS.rcLocal)) {
+    copyFileSync(CLASP_PATHS.rcLocal, `${CLASP_PATHS.rcLocal}~`);
+  }
+  if (fs.existsSync(CLASP_PATHS.settingsLocal)) {
+    copyFileSync(CLASP_PATHS.settingsLocal, `${CLASP_PATHS.settingsLocal}~`);
+  }
+};
+
+export const restoreSettings = () => {
+  if (fs.existsSync(`${CLASP_PATHS.rcGlobal}~`)) {
+    fs.renameSync(`${CLASP_PATHS.rcGlobal}~`, CLASP_PATHS.rcGlobal);
+  }
+  if (fs.existsSync(`${CLASP_PATHS.rcLocal}~`)) {
+    fs.renameSync(`${CLASP_PATHS.rcLocal}~`, CLASP_PATHS.rcLocal);
+  }
+  if (fs.existsSync(`${CLASP_PATHS.settingsLocal}~`)) {
+    fs.renameSync(`${CLASP_PATHS.settingsLocal}~`, CLASP_PATHS.settingsLocal);
+  }
+};
