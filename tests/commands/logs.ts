d54980d5fa3d580831632bbaf21db6f23b077742
campionfellin <campionfellin@gmail.com>
2019-02-26 20:51:43 -0800

    Add more tests
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- tests/commands/logs.ts
+++ tests/commands/logs.ts
@@ -2,29 +2,23 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 const { spawnSync } = require('child_process');
 
-import {
-  CLASP,
-  SCRIPT_ID,
-} from '../constants';
+import { CLASP } from '../constants';
 
 import {
   cleanup,
+  setup,
   setupWithoutGCPProject,
 } from '../functions';
 
-import {
-  LOG,
-} from '../../src/utils';
+import { LOG } from '../../src/utils';
 
-describe('Test clasp logs function', () => {
+describe('Test clasp logs setup', () => {
   before(setupWithoutGCPProject);
   it('should prompt for logs setup', () => {
     const result = spawnSync(
       CLASP, ['logs'], { encoding: 'utf8' },
     );
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.contain(`${LOG.OPEN_LINK(LOG.SCRIPT_LINK(SCRIPT_ID))}\n`);
-    expect(result.stdout).to.contain(`${LOG.GET_PROJECT_ID_INSTRUCTIONS}\n`);
     expect(result.stdout).to.contain(`${LOG.ASK_PROJECT_ID}`);
   });
   it('should prompt for logs setup', () => {
@@ -32,9 +26,23 @@ describe('Test clasp logs function', () => {
       CLASP, ['logs', '--setup'], { encoding: 'utf8' },
     );
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.contain(`${LOG.OPEN_LINK(LOG.SCRIPT_LINK(SCRIPT_ID))}\n`);
-    expect(result.stdout).to.contain(`${LOG.GET_PROJECT_ID_INSTRUCTIONS}\n`);
     expect(result.stdout).to.contain(`${LOG.ASK_PROJECT_ID}`);
+  });
+  after(cleanup);
+});
+
+describe('Test clasp logs function', () => {
+  before(setup);
+  it('should get some logs', () => {
+    const result = spawnSync(
+      CLASP, ['logs'], { encoding: 'utf8' },
+    );
+    expect(result.status).to.equal(0);
+     // Example log line:
+     // tslint:disable-next-line:max-line-length
+     // NOTICE               2019-02-26T05:10:20.658Z google.api.serviceusage.v1.ServiceUsage.EnableService Setting up StackDriver Logging.
+     // ... we'll just expect there to be *some* stdout.
+    expect(result.stdout).lengthOf.greaterThan(10);
   });
   after(cleanup);
 });
