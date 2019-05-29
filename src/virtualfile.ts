4e37b4ad9045e05ab1e930c89b07c56868beefe0
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-05-29 16:01:06 +0200

    doc + code cleanup

--- src/virtualfile.ts
+++ src/virtualfile.ts
@@ -9,95 +9,110 @@ export class VirtualFile {
   protected _defaultPath: SplitPath;
 
   /**
-   * Handles a file which may not exists. With a default path (which can be overrid)
+   * Handles a file which may not exists.
+   * The file has a default path, which can be overridden by the `path` accessor.
+   * The `resolve()` method implements specific rules to define the effective path of  of the virtual file.
    *
    * @param {SplitPath} defaultPath default path
    */
   constructor(defaultPath: SplitPath) {
-    const { dir, base } = defaultPath;
-    this._defaultPath = { dir, base };
+    this._defaultPath = defaultPath;
   }
 
   /**
-   * Returns the current defined path
+   * Returns the current (raw and unresolved) defined path of the virtual file.
+   *
+   * *Note: for most uses, prefer the `resolve()` method in order to retreive a file's path*
+   *
+   * @returns {string}
    */
   get path() {
     return this.userDefinedPath || path.join(this._defaultPath.dir, this._defaultPath.base);
   }
 
   /**
-   * Sets the current path
+   * Sets the current (raw and unresolved) path of the virtual file.
    *
-   * *Pro-tip: passing an empty string re-enable the default path*
+   * *Note: passing an empty string restores the default path*
    */
   set path(userDefinedPath: string ) {
     this.userDefinedPath = userDefinedPath;
   }
 
+  /**
+   * Returns the resolved directory of the virtual file.
+   *
+   * *Note: for most uses, prefer the `.resolve()` method in order to retreive a file's path*
+   *
+   * @returns {string}
+   */
   get resolvedDir() {
     return path.dirname(this.resolve());
   }
 
   /**
-   * Returns true if current path is the default path
+   * Returns true if current path is the default.
+   *
+   * @returns {boolean}
    */
   isDefault() {
     return !this.userDefinedPath;
   }
 
   /**
-   * Attempts to resolve the current active path
+   * Resolves the current active path
+   *
+   * @returns {string}
    */
   resolve() {
-    if (this.userDefinedPath) {
-      return resolvePath(this.userDefinedPath, this._defaultPath.base);
-    }
-    return path.join(this._defaultPath.dir, this._defaultPath.base);
+    return this.userDefinedPath
+      ? resolvePath(this.userDefinedPath, this._defaultPath.base)
+      : path.join(this._defaultPath.dir, this._defaultPath.base);
   }
+
+  // TODO: consider hosting the `DOTFILE` function for simplicity sake.
 }
 
 /**
- * Attempts to resolve a path as follow:
+ * Attempts to resolve a path with the following rules:
  *
- * - path exists and points to a file: use it as is
- * - path exists and points to a directory: append the default base filename to the path
- * - path partially resolves to an existing directory but base filename does not exists: use it as is
+ * - if path exists and points to a file: use it as is
+ * - if path exists and points to a directory: append the default base filename to the path
+ * - if path partially resolves to an existing directory but base filename does not exists: use it as is
  * - otherwise throw an error
  *
- * @param pathToResolve the path to resolve
- * @param base the default base filename
+ * @param {string} pathToResolve the path to resolve
+ * @param {string} base the default base filename
+ *
+ * @returns {string}
  */
 const resolvePath = (pathToResolve: string, base: string) => {
   if (fs.existsSync(pathToResolve)) {
     return appendBaseIfDirectory(pathToResolve, base);
   }
   const parsedPath = path.parse(pathToResolve);
-  if (parsedPath.dir) {
-    if (fs.existsSync(parsedPath.dir) && fs.lstatSync(parsedPath.dir).isDirectory()) {
-      return pathToResolve; // assume fullpath to missing file
-    }
+  if (parsedPath.dir === '' || fs.lstatSync(parsedPath.dir).isDirectory()) {
+    return pathToResolve; // assume fullpath to missing file
   }
   // TODO: improve support for unresolved paths
   throw new Error(`Unrecognized path ${pathToResolve}`);
 };
 
 /**
- * Attempts to resolve an **existing** path as follow:
+ * Attempts to resolve an **existing** path with the following rules:
  *
- * - path points to a file: use it as is
- * - path points to a directory: append the default base filename to the path
+ * - if path points to a file: use it as is
+ * - if path points to a directory: append the default base filename to the path
  * - otherwise throw an error
  *
- * @param somePath the path to resolve
- * @param base the default base filename
+ * @param {string} somePath the path to resolve
+ * @param {string} base the default base filename
+ *
+ * @returns {string}
  */
 const appendBaseIfDirectory = (somePath: string, base: string ) => {
   const stats = fs.lstatSync(somePath);
-  if (stats.isFile()) {
-    return somePath;
-  }
-  if (stats.isDirectory()) {
-    return path.join(somePath, base);
-  }
+  if (stats.isFile()) return somePath;
+  if (stats.isDirectory()) return path.join(somePath, base);
   throw new Error(`Unrecognized path ${somePath}`);
 };
