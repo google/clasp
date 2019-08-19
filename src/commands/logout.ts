6303ae11cc13d93e117c8bc2c1c0a834979da0ba
Michael Archer <mikearche@gmail.com>
2019-08-19 11:10:16 -0700

    Issue 668: Fix logging out on windows (#684)
    * Fix bug where logout didn't work on windows
    
    * Remove unnecessary comment
    
    * remove trailing whitespace

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -1,12 +1,11 @@
 import { DOT } from '../dotfile';
-import del from 'del';
+import fs from 'fs-extra';
 import { hasOauthClientSettings } from '../utils';
 
 /**
  * Logs out the user by deleting credentials.
  */
 export default async () => {
-  if (hasOauthClientSettings(true)) del(DOT.RC.ABSOLUTE_LOCAL_PATH, { force: true });
-  // del doesn't work with a relative path (~)
-  if (hasOauthClientSettings()) del(DOT.RC.ABSOLUTE_PATH, { force: true });
+  if (hasOauthClientSettings(true)) fs.unlinkSync(DOT.RC.ABSOLUTE_LOCAL_PATH);
+  if (hasOauthClientSettings()) fs.unlinkSync(DOT.RC.ABSOLUTE_PATH);
 };
