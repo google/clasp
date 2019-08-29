02e1ee1cc764be4859e51c5e2c5e688653ac80d6
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-08-29 17:32:07 +0200

    doc doc

--- tests/functions.ts
+++ tests/functions.ts
@@ -9,6 +9,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import tmp from 'tmp';
 
+/** basic cleanup after tests */
 export const cleanup = () => {
   fs.removeSync('.clasp.json');
   fs.removeSync('.claspignore');
@@ -16,23 +17,30 @@ export const cleanup = () => {
   fs.removeSync('appsscript.json');
 };
 
+/** basic setup for tests */
 export const setup = () => {
   fs.writeFileSync('.clasp.json', CLASP_SETTINGS.valid);
   fs.writeFileSync('appsscript.json', TEST_APPSSCRIPT_JSON_WITHOUT_RUN_API);
 };
 
+/** setup for tests not using the run API */
 export const setupWithoutGCPProject = () => {
   fs.writeFileSync('.clasp.json', CLASP_SETTINGS.validWithoutProjectId);
   fs.writeFileSync('appsscript.json', TEST_APPSSCRIPT_JSON_WITHOUT_RUN_API);
 };
 
+/** setup for tests using the run API */
 export const setupWithRunManifest = () => {
   fs.writeFileSync('.clasp.json', CLASP_SETTINGS.valid);
   fs.writeFileSync('appsscript.json', TEST_APPSSCRIPT_JSON_WITH_RUN_API);
 };
 
+/** produce a pseudo random string */
 export const rndStr = () => Math.random().toString(36).substr(2);
 
+/**
+ * backup clasp settings. Use `restoreSettings()` to restore these.
+ */
 export const backupSettings = () => {
   if (fs.existsSync(CLASP_PATHS.rcGlobal)) {
     fs.copyFileSync(CLASP_PATHS.rcGlobal, `${CLASP_PATHS.rcGlobal}~`);
@@ -45,6 +53,9 @@ export const backupSettings = () => {
   }
 };
 
+/**
+ * restore clasp settings backuped up using `backupSettings()`
+ */
 export const restoreSettings = () => {
   if (fs.existsSync(`${CLASP_PATHS.rcGlobal}~`)) {
     fs.renameSync(`${CLASP_PATHS.rcGlobal}~`, CLASP_PATHS.rcGlobal);
@@ -57,6 +68,11 @@ export const restoreSettings = () => {
   }
 };
 
+/**
+ * create a temporary directory and its content, then return its path as a string
+ *
+ * @param {Array<{ file: string, data: string }} filepathsAndContents directory content (files)
+ */
 export function setupTmpDirectory(filepathsAndContents: Array<{ file: string, data: string }>) {
   fs.ensureDirSync('tmp');
   const tmpdir = tmp.dirSync({ unsafeCleanup: true, dir: 'tmp/', keep: false }).name;
