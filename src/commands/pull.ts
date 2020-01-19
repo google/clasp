af914d2db04165617c6e52b8f34d895d83081251
PopGoesTheWza <popgoesthewza@gmail.com>
2020-01-19 21:37:02 +0100

    linting

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -6,12 +6,13 @@ import { LOG, checkIfOnline, getProjectSettings, spinner } from '../utils';
  * @param cmd.versionNumber {number} The version number of the project to retrieve.
  *                             If not provided, the project's HEAD version is returned.
  */
-export default async (cmd: { versionNumber: number }) => {
+export default async (cmd: { versionNumber: number }): Promise<void> => {
   await checkIfOnline();
   const { scriptId, rootDir } = await getProjectSettings();
   if (scriptId) {
     spinner.setSpinnerTitle(LOG.PULLING);
     const files = await fetchProject(scriptId, cmd.versionNumber);
     await writeProjectFiles(files, rootDir);
+    spinner.stop(true);
   }
 };
