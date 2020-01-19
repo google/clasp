22a15675f02a36edf14ec58aae5b62635ff68b06
PopGoesTheWza <popgoesthewza@gmail.com>
2020-01-19 21:06:23 +0100

    linting

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -5,7 +5,7 @@ import { hasOauthClientSettings } from '../utils';
 /**
  * Logs out the user by deleting credentials.
  */
-export default async () => {
+export default async (): Promise<void> => {
   if (hasOauthClientSettings(true)) fs.unlinkSync(DOT.RC.ABSOLUTE_LOCAL_PATH);
   if (hasOauthClientSettings()) fs.unlinkSync(DOT.RC.ABSOLUTE_PATH);
 };
