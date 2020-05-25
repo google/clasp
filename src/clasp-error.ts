e9a061af3f776509717a7e762e25af9a9ecf43cb
PopGoesTheWza <popgoesthewza@gmail.com>
2020-05-25 17:50:38 +0200

    style: comments

--- src/clasp-error.ts
+++ src/clasp-error.ts
@@ -2,8 +2,8 @@ export class ClaspError extends Error {
   constructor(message: string, exitCode = 1) {
     super(message);
     // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
-    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
-    this.name = ClaspError.name; // stack traces display correctly now
+    Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
+    this.name = ClaspError.name; // Stack traces display correctly now
     process.exitCode = exitCode;
   }
 }
