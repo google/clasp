255bc179fc049dde5e4fb69f44f0ad8f065f6e6d
PopGoesTheWza <popgoesthewza@gmail.com>
2021-03-24 20:40:41 +0100

    chore: bump dependencies

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,11 +7,3 @@ declare module 'normalize-newline' {
   function normalizeNewline(inout: string | Buffer): string;
   export = normalizeNewline;
 }
-
-declare module 'split-lines' {
-  interface Options {
-    readonly preserveNewlines?: boolean;
-  }
-  function splitLines(inout: string, options?: Options): string[];
-  export = splitLines;
-}
