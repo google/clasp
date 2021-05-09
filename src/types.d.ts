b3945fe8240b4bf811a0a3c580cc9a5e67a8ec36
PopGoesTheWza <popgoesthewza@gmail.com>
2021-05-09 12:19:59 +0200

    chore: improved type declarations

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,20 @@ type Unpacked<T> = T extends Array<infer U> ? U : T;
 declare module 'inquirer-autocomplete-prompt-ipt';
 
 declare module 'normalize-newline' {
-  function normalizeNewline(inout: string | Buffer): string;
+  /**
+Normalize the newline characters in a string to `\n`.
+@example
+```
+import normalizeNewline from 'normalize-newline';
+
+normalizeNewline('foo\r\nbar\nbaz');
+//=> 'foo\nbar\nbaz'
+
+normalizeNewline(Buffer.from('foo\r\nbar\nbaz')).toString();
+//=> 'foo\nbar\nbaz'
+```
+  */
+  function normalizeNewline(input: string): string;
+  function normalizeNewline(input: Buffer): Buffer;
   export = normalizeNewline;
 }
