29b925e5d97a0a426d254be7ac0a91344d05c17a
HoldYourWaffle <ravivanrooijen@live.nl>
2023-11-06 04:43:06 +0100

    Throw error if SCRIPT/PROJECT_ID is not defined
    As far as I can tell not-defining these will always lead to trouble later.
    Is `ClaspError` appropriate here?

--- test/constants.ts
+++ test/constants.ts
@@ -2,6 +2,7 @@ import os from 'os';
 import path from 'path';
 
 import {ClaspToken} from '../src/dotfile.js';
+import {ClaspError} from '../src/clasp-error.js';
 import {randomString} from './functions.js';
 
 import type {OAuth2ClientOptions} from 'google-auth-library';
@@ -27,9 +28,15 @@ export const TEST_APPSSCRIPT_JSON_WITH_RUN_API = JSON.stringify({
 
 // Travis Env Variables
 export const IS_PR: boolean = process.env.CI === 'true';
-export const SCRIPT_ID: string = process.env.SCRIPT_ID ?? '';
-export const PROJECT_ID: string = process.env.PROJECT_ID ?? '';
-export const PARENT_ID: string[] = [process.env.PROJECT_ID ?? ''];
+if (process.env.SCRIPT_ID === undefined) {
+  throw new ClaspError('Environment variable SCRIPT_ID is not defined.');
+}
+export const SCRIPT_ID: string = process.env.SCRIPT_ID;
+if (process.env.PROJECT_ID === undefined) {
+  throw new ClaspError('Environment variable PROJECT_ID is not defined.');
+}
+export const PROJECT_ID: string = process.env.PROJECT_ID;
+export const PARENT_ID: string[] = [process.env.PROJECT_ID];
 const HOME: string = process.env.HOME ?? '';
 
 // Paths
