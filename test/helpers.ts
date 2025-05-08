24749ad3782ecf74168b71fb8397afbfb84af899
Steven Bazyl <sqrrrl@gmail.com>
2025-05-08 11:59:52 -0600

    wqchore: Updated depdencies, migrate from chai-subset to new built-in method

--- test/helpers.ts
+++ test/helpers.ts
@@ -2,11 +2,9 @@ import * as fs from 'fs';
 import * as path from 'path';
 import {use} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import chaiSubset from 'chai-subset';
 
 export function useChaiExtensions() {
   use(chaiAsPromised);
-  use(chaiSubset);
   use(chaiFileExists);
 }
 
