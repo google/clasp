892f6cdc431f453a2cc48c1d59ba67ff700bddf3
fantonangeli <fabrizio.antonangeli@gmail.com>
2025-05-15 23:35:28 +0200

    Add tests with prompt

--- test/commands/delete.ts
+++ test/commands/delete.ts
@@ -7,6 +7,8 @@ import mockfs from 'mock-fs';
 import {useChaiExtensions} from '../helpers.js';
 import {mockOAuthRefreshRequest, mockTrashScript, resetMocks, setupMocks} from '../mocks.js';
 import {runCommand} from './utils.js';
+import sinon from 'sinon';
+import inquirer from 'inquirer';
 
 useChaiExtensions();
 
@@ -38,6 +40,23 @@ describe('Delete script command', function () {
         scriptId: 'mock-script-id',
       });
       const out = await runCommand(['delete', '-f']);
+      expect(out.stdout).to.contain('Deleted script mock-script-id');
+    });
+
+    it('should not delete the script if user declines', async function () {
+      mockTrashScript({
+        scriptId: 'mock-script-id',
+      });
+      sinon.stub(inquirer, 'prompt').resolves({ answer: false });
+      const out = await runCommand(['delete']);
+      expect(out.stdout).to.contain('Are you sure you want to delete');
+      expect(out.stdout).to.not.contain('Deleted script');
+    });
+
+    it('should delete the script if user confirms', async function () {
+      mockTrashScript({ scriptId: 'mock-script-id' });
+      sinon.stub(inquirer, 'prompt').resolves({ answer: false });
+      const out = await runCommand(['delete']);
       expect(out.stdout).to.contain('Deleted script mock-script-id');
     });
   });
